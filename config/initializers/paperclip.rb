4e1bbfe631986d504ee4f9963087a5444c2d7f91
Julian Weber <jweber@avarteq.de>
2014-04-04 03:41:44 +0200

    changes initializer to use paperclip

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -3,5 +3,5 @@ BUCKET_NAME = "images"
 if Rails.env.production?
   require 'paperclip'
   require 'a9s_swift'
-  Anynines::Swift::Utility.configure_carrierwave(BUCKET_NAME)
+  Anynines::Swift::Utility.configure_paperclip(BUCKET_NAME)
 end
